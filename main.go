775b17c2609193f5880b36309d163385951c17eb
belyaevEDU <veniamin@belyaev.work>
2026-08-07 19:20:53 +0300

    func(main): added manual testing of impltd run & stop

--- main.go
+++ main.go
@@ -2,65 +2,63 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/belyaevedu/philharmonic/manager"
-	"github.com/belyaevedu/philharmonic/node"
 	"github.com/belyaevedu/philharmonic/task"
-	"github.com/belyaevedu/philharmonic/worker"
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
+	"github.com/moby/moby/client"
 )
 
 func main() {
-	t := task.Task{
-		ID:     uuid.New(),
-		Name:   "Task-1",
-		State:  task.Pending,
-		Image:  "image-1",
-		Memory: 1024,
-		Disk:   1,
+	dockerTask, createResult := createContainer()
+	if createResult != nil && createResult.Error != nil {
+		fmt.Printf("%v", createResult.Error)
+		os.Exit(1)
 	}
 
-	te := task.TaskEvent{
-		ID:        uuid.New(),
-		State:     task.Pending,
-		Timestamp: time.Now(),
-		Task:      t,
+	fmt.Println("stopping")
+
+	time.Sleep(time.Second * 5)
+	fmt.Printf("stopping cntnr %s\n", createResult.ContainerId)
+	if dockerTask == nil {
+		fmt.Println("dockertask is nil")
+	}
+	_ = stopContainer(dockerTask, createResult.ContainerId)
+}
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:latest",
+		Env: []string{
+			"POSTGRES_USER=philharmonic_cube",
+			"POSTGRES_PASSWORD=secret",
+		},
 	}
 
-	fmt.Printf("task: %v\n", t)
-	fmt.Printf("task event: %v\n", te)
-	w := worker.Worker{
-		Name:  "worker-1",
-		Queue: *queue.New(),
-		Db:    make(map[uuid.UUID]*task.Task),
+	dc, _ := client.New(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
 	}
-	fmt.Printf("worker: %v\n", w)
 
-	w.CollectStats()
-	w.RunTask()
-	w.StartTask()
-	w.StopTask()
-
-	m := manager.Manager{
-		Pending: *queue.New(),
-		TaskDb:  make(map[string][]*task.Task),
-		EventDb: make(map[string][]*task.TaskEvent),
-		Workers: []string{w.Name},
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
 	}
-	fmt.Printf("manager: %v\n", m)
-	m.SelectWorker()
-	m.UpdateTasks()
-	m.SendTask()
 
-	n := node.Node{
-		Name:   "Node-1",
-		Ip:     "192.168.1.1",
-		Cores:  4,
-		Memory: 1024,
-		Disk:   25,
-		Role:   "worker",
+	fmt.Printf("Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
+
+func stopContainer(d *task.Docker, id string) *task.DockerResult {
+	result := d.Stop(id)
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil
 	}
-	fmt.Printf("node: %v\n", n)
+
+	fmt.Printf("Container %s has been stopped and removed\n", result.ContainerId)
+	return &result
 }
