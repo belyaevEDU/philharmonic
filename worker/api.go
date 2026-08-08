de003e63c661cd50308c188f98fc4a4cfa61491e
belyaevEDU <veniamin@belyaev.work>
2026-08-08 14:54:15 +0300

    func(worker): implemented structs for the worker http rest api

---
+++ worker/api.go
@@ -0,0 +1,15 @@
+package worker
+
+import "github.com/go-chi/chi/v5"
+
+type Api struct {
+	Address string
+	Port    int
+	Worker  *Worker
+	Router  chi.Mux
+}
+
+type HTTPErrResponse struct {
+	HTTPStatusCode int
+	Message        string
+}
