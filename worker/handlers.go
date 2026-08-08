d435f22d396d4ed848ed65b9021e703bbe07c600
belyaevEDU <veniamin@belyaev.work>
2026-08-08 15:52:58 +0300

    func(worker): implemented an httpErrResponse helper to reduce code duplication

--- worker/handlers.go
+++ worker/handlers.go
@@ -17,13 +17,7 @@ func (a *Api) StartTaskHandler(w http.ResponseWriter, r *http.Request) {
 	err := d.Decode(&te)
 	if err != nil {
 		msg := fmt.Sprintf("Error unmarshalling body: %v\n", err)
-		log.Print(msg)
-		w.WriteHeader(http.StatusBadRequest)
-		her := HTTPErrResponse{
-			HTTPStatusCode: http.StatusBadRequest,
-			Message:        msg,
-		}
-		json.NewEncoder(w).Encode(her)
+		httpErrResponseHelper(w, msg, http.StatusBadRequest)
 		return
 	}
 
@@ -37,4 +31,17 @@ func (a *Api) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(a.Worker.getTasks())
+}
+
+func httpErrResponseHelper(w http.ResponseWriter, message string, statusCode int) {
+	// moved the log here because i think for every call of this request
+	// im either going to log the message or not
+	// regardless of the handler
+	log.Print(message)
+	w.WriteHeader(statusCode)
+	her := HTTPErrResponse{
+		HTTPStatusCode: statusCode,
+		Message:        message,
+	}
+	json.NewEncoder(w).Encode(her)
 }
