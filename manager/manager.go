d85e07c7f08d5bcd6b9987592f0a04bc0d8dfc5c
belyaevEDU <veniamin@belyaev.work>
2026-08-09 11:38:36 +0300

    feat(manager): implemented worker selection via round-robin

--- manager/manager.go
+++ manager/manager.go
@@ -13,10 +13,22 @@ type Manager struct {
 	Workers       []string
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+	LastWorker    int
 }
 
-func (m *Manager) SelectWorker() {
-	// selects worker by reqs
+func (m *Manager) SelectWorker() string {
+	// naive round-robin
+	// e-pvm later
+
+	var newWorker int
+	if m.LastWorker+1 < len(m.Workers) {
+		newWorker = m.LastWorker + 1
+	} else {
+		m.LastWorker = 0
+	}
+	m.LastWorker = newWorker
+
+	return m.Workers[newWorker]
 }
 
 func (m *Manager) UpdateTasks() {
