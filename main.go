cf3755c54cfa4c5d118b2aef93ba414fc349edce
belyaevEDU <veniamin@belyaev.work>
2026-08-09 14:59:43 +0300

    feat(main): added a manager instance to main for manual testing

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/belyaevedu/philharmonic/manager"
 	"github.com/belyaevedu/philharmonic/task"
 	"github.com/belyaevedu/philharmonic/worker"
 	"github.com/golang-collections/collections/queue"
@@ -21,8 +22,6 @@ func main() {
 		return
 	}
 
-	fmt.Println("starting worker")
-
 	w := worker.Worker{
 		Queue: *queue.New(),
 		Db:    make(map[uuid.UUID]*task.Task),
@@ -31,9 +30,50 @@ func main() {
 
 	go runTasks(&w)
 	go w.CollectStats()
-	err = api.Start()
-	if err != nil {
-		fmt.Printf("Error raised when starting the http server: %v", err)
+	go func() {
+		err = api.Start()
+		if err != nil {
+			fmt.Printf("Error raised when starting the http server: %v", err)
+			os.Exit(1)
+		}
+	}()
+
+	// api.Start()
+	time.Sleep(3 * time.Second)
+
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(workers)
+
+	for i := 0; i < 3; i++ {
+		t := task.Task{
+			ID:    uuid.New(),
+			Name:  fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "strm/helloworld-http",
+		}
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+		m.AddTask(te)
+		m.SendTask()
+	}
+
+	go func() {
+		for {
+			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+			m.UpdateTasks()
+
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDb {
+			fmt.Printf("[Manager] Task: id %s, state %d\n", t.ID, t.State)
+			time.Sleep(10 * time.Second)
+		}
 	}
 }
 
@@ -45,9 +85,8 @@ func runTasks(w *worker.Worker) {
 				log.Printf("Error running task: %v\n", result.Error)
 			}
 		} else {
-			log.Printf("No tasks to process currently.\n")
+			log.Println("No tasks to process currently.")
 		}
-		log.Println("sleeping")
 		time.Sleep(time.Second * 10)
 	}
 }
