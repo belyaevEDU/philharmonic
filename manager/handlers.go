03aacd42c76ee7e715beb55731462572a9443f77
belyaevEDU <veniamin@belyaev.work>
2026-08-10 14:26:35 +0300

    func(manager/handlers): added handlers for starting/stopping tasks and getting all of them

---
+++ manager/handlers.go
@@ -0,0 +1,85 @@
+package manager
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/belyaevedu/philharmonic/handlers"
+	"github.com/belyaevedu/philharmonic/task"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
+
+func (a *Api) StartTaskHandler(w http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+
+	te := task.TaskEvent{}
+	err := d.Decode(&te)
+	if err != nil {
+		msg := fmt.Sprintf(handlers.ErrorUnmarshallingJson, err)
+		err = handlers.HttpResponseHelper(w, msg, http.StatusBadRequest)
+		if err != nil {
+			log.Printf(handlers.ErrorEncodingJson, err.Error())
+		}
+		return
+	}
+
+	a.Manager.AddTask(te)
+	log.Printf("Added task %v\n", te.Task.ID)
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(te.Task)
+	if err != nil {
+		log.Printf(handlers.ErrorEncodingJsonWithTaskID, te.Task.ID.String(), err.Error())
+	}
+}
+
+func (a *Api) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err := json.NewEncoder(w).Encode(a.Manager.getTasks())
+	if err != nil {
+		log.Printf(handlers.ErrorEncodingJson, err.Error())
+	}
+}
+
+func (a *Api) StopTaskHandler(w http.ResponseWriter, r *http.Request) {
+	taskID := chi.URLParam(r, "taskID")
+	if taskID == "" {
+		log.Println("No taskID passed in the request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tID, err := uuid.Parse(taskID)
+	if err != nil {
+		log.Println("Non-UUID taskID passed in the request.")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	taskToStop, exists := a.Manager.TaskDb[tID]
+	if !exists {
+		log.Printf("No task with ID %v found\n", tID)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	taskCopy := *taskToStop
+	taskCopy.State = task.Completed
+
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Completed,
+		Timestamp: time.Now(),
+		Task:      taskCopy,
+	}
+
+	a.Manager.AddTask(te)
+
+	log.Printf("Added task %v to stop container %v\n", taskToStop.ID, taskToStop.ContainerID)
+	w.WriteHeader(http.StatusNoContent)
+}
