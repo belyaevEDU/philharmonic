6f1cbf375d2575282c9375511f416b03e5287f2a
belyaevEDU <veniamin@belyaev.work>
2026-08-10 14:14:37 +0300

    func(manager): added api.go with structs for the manager API and response with msg+statuscode

---
+++ manager/api.go
@@ -0,0 +1,15 @@
+package manager
+
+import "github.com/go-chi/chi/v5"
+
+type Api struct {
+	Address string
+	Port    int
+	Manager *Manager
+	Router  *chi.Mux
+}
+
+type HTTPResponse struct {
+	HTTPStatusCode int
+	Message        string
+}
