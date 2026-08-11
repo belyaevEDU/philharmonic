6bb07669da9764b06d1655d647201da6047ac8eb
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2026-08-12 01:09:15 +0300

    style(manager/manager): shifted the order of the methods around to have the fundamentals at the top

--- manager/manager.go
+++ manager/manager.go
@@ -78,57 +78,6 @@ func (m *Manager) getTasks() []*task.Task {
 	return tasks
 }
 
-func (m *Manager) updateTasks() {
-	for _, worker := range m.Workers {
-		log.Printf("Checking worker %v for task updates\n", worker)
-
-		url := fmt.Sprintf(WorkerTasksURL, worker)
-		// ignoring gosec's G107 since the url is not from external input, but from an internal config
-		resp, err := http.Get(url) // #nosec G107
-		if err != nil {
-			log.Printf("Error connecting to %s: %v\n", worker, err)
-			continue
-		}
-
-		if resp.StatusCode != http.StatusOK {
-			log.Printf("Error sending request to worker %s: %v\n", worker, err)
-			continue
-		}
-
-		d := json.NewDecoder(resp.Body)
-		var tasks []*task.Task
-		err = d.Decode(&tasks)
-		if err != nil {
-			log.Printf("Error unmarshalling tasks from %s: %v", worker, err)
-			continue
-		}
-
-		for _, t := range tasks {
-			log.Printf("Attempting to update task %s\n", t.ID.String())
-
-			_, ok := m.TaskDb[t.ID]
-			if !ok {
-				log.Printf("Task with ID %s not found\n", t.ID.String())
-				return
-			}
-
-			m.TaskDb[t.ID].State = t.State
-			m.TaskDb[t.ID].StartTime = t.StartTime
-			m.TaskDb[t.ID].FinishTime = t.FinishTime
-			m.TaskDb[t.ID].ContainerID = t.ContainerID
-		}
-	}
-}
-
-func (m *Manager) UpdateTasks() {
-	for {
-		log.Println("[Manager] Checking for task updates from workers")
-		m.updateTasks()
-		log.Println("Task updates completed. Sleeping for 10 seconds")
-		time.Sleep(10 * time.Second)
-	}
-}
-
 func (m *Manager) SendWork() {
 	if m.Pending.Len() > 0 {
 		w := m.SelectWorker()
@@ -187,6 +136,57 @@ func (m *Manager) SendWork() {
 		log.Printf("%#v\n", t) // # adds field names
 	} else {
 		log.Println("No work in the queue")
+	}
+}
+
+func (m *Manager) updateTasks() {
+	for _, worker := range m.Workers {
+		log.Printf("Checking worker %v for task updates\n", worker)
+
+		url := fmt.Sprintf(WorkerTasksURL, worker)
+		// ignoring gosec's G107 since the url is not from external input, but from an internal config
+		resp, err := http.Get(url) // #nosec G107
+		if err != nil {
+			log.Printf("Error connecting to %s: %v\n", worker, err)
+			continue
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Error sending request to worker %s: %v\n", worker, err)
+			continue
+		}
+
+		d := json.NewDecoder(resp.Body)
+		var tasks []*task.Task
+		err = d.Decode(&tasks)
+		if err != nil {
+			log.Printf("Error unmarshalling tasks from %s: %v", worker, err)
+			continue
+		}
+
+		for _, t := range tasks {
+			log.Printf("Attempting to update task %s\n", t.ID.String())
+
+			_, ok := m.TaskDb[t.ID]
+			if !ok {
+				log.Printf("Task with ID %s not found\n", t.ID.String())
+				return
+			}
+
+			m.TaskDb[t.ID].State = t.State
+			m.TaskDb[t.ID].StartTime = t.StartTime
+			m.TaskDb[t.ID].FinishTime = t.FinishTime
+			m.TaskDb[t.ID].ContainerID = t.ContainerID
+		}
+	}
+}
+
+func (m *Manager) UpdateTasks() {
+	for {
+		log.Println("[Manager] Checking for task updates from workers")
+		m.updateTasks()
+		log.Println("Task updates completed. Sleeping for 10 seconds")
+		time.Sleep(10 * time.Second)
 	}
 }
 
