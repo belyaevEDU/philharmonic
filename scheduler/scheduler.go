896c03ea0a7893e8f5146b54fc08569d80a70564
belyaevEDU <veniamin@belyaev.work>
2026-08-13 13:43:11 +0300

    feat(scheduler): updated the signatures of the scheduler interface's methods

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,12 @@
 package scheduler
 
+import (
+	"github.com/belyaevedu/philharmonic/node"
+	"github.com/belyaevedu/philharmonic/task"
+)
+
 type Scheduler interface {
-	SelectCandidateNodes()
-	Score()
-	Pick()
+	SelectCandidateNodes(t *task.Task, nodes []*node.Node) []*node.Node
+	Score(t *task.Task, nodes []*node.Node) map[string]float64
+	Pick(scores map[string]float64, candidates []*node.Node) *node.Node
 }
