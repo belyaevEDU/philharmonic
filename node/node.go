d60f851c85914d96378c3db7bc805323eb270edc
belyaevEDU <veniamin@belyaev.work>
2026-08-14 00:30:12 +0300

    refactor: moved getNodeStats to node as a method for the Node struct

--- node/node.go
+++ node/node.go
@@ -1,6 +1,20 @@
 package node
 
-import "github.com/belyaevedu/philharmonic/stats"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/belyaevedu/philharmonic/stats"
+	"github.com/belyaevedu/philharmonic/utils"
+)
+
+const (
+	StatsQueryMaxRetries  = 5
+	StatsQuerySleepPeriod = 5
+)
 
 type Node struct {
 	Name            string // ip:port
@@ -21,4 +35,34 @@ func NewNode(name, api, role string) *Node {
 		Api:  api,
 		Role: role,
 	}
+}
+
+func (n *Node) GetStats() (*stats.Stats, error) {
+	url := fmt.Sprintf("%s/stats", n.Api)
+	resp, err := utils.HTTPWithRetry(http.Get, url, StatsQueryMaxRetries, StatsQuerySleepPeriod) // #nosec G107
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to %v: %w", n.Api, err)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("error retrieving stats from %v: %w", n.Api, err)
+	}
+
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Printf("Error closing response body: %v\n", err)
+		}
+	}()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading stats resp body from %v: %w", n.Api, err)
+	}
+
+	var stats stats.Stats
+	if err := json.Unmarshal(body, &stats); err != nil {
+		return nil, fmt.Errorf("error unmarshalling body of stats from %v: %w", n.Api, err)
+	}
+	return &stats, nil
 }
--- scheduler/stats_helpers.go
+++ scheduler/stats_helpers.go
@@ -1,15 +1,9 @@
 package scheduler
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"time"
 
 	"github.com/belyaevedu/philharmonic/node"
-	"github.com/belyaevedu/philharmonic/stats"
 	"github.com/belyaevedu/philharmonic/task"
 )
 
@@ -25,14 +19,14 @@ func calculateCpuUsage(node *node.Node) (*float64, error) {
 	// not using the Stats.CpuUsage method
 	// because the calculation is a bit different
 
-	stat1, err := getNodeStats(node)
+	stat1, err := node.GetStats()
 	if err != nil {
 		return nil, err
 	}
 
 	time.Sleep(3 * time.Second)
 
-	stat2, err := getNodeStats(node)
+	stat2, err := node.GetStats()
 	if err != nil {
 		return nil, err
 	}
@@ -53,34 +47,4 @@ func calculateCpuUsage(node *node.Node) (*float64, error) {
 		cpuPercentUsage = (float64(total) - float64(idle)) / float64(total)
 	}
 	return &cpuPercentUsage, nil
-}
-
-func getNodeStats(node *node.Node) (*stats.Stats, error) {
-	url := fmt.Sprintf("%s/stats", node.Api)
-	resp, err := http.Get(url) // #nosec G107
-	if err != nil {
-		return nil, fmt.Errorf("error connecting to %v: %w", node.Api, err)
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("error retrieving stats from %v: %w", node.Api, err)
-	}
-
-	defer func() {
-		err := resp.Body.Close()
-		if err != nil {
-			log.Printf("Error closing response body: %v\n", err)
-		}
-	}()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading stats resp body from %v: %w", node.Api, err)
-	}
-
-	var stats stats.Stats
-	if err := json.Unmarshal(body, &stats); err != nil {
-		return nil, fmt.Errorf("error unmarshalling body of stats from %v: %w", node.Api, err)
-	}
-	return &stats, nil
 }
