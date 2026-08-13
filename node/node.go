e2c14c08413fa63094c58b957aba62389dfc699e
belyaevEDU <veniamin@belyaev.work>
2026-08-13 14:37:04 +0300

    feat(node): added a NewNode method

--- node/node.go
+++ node/node.go
@@ -11,3 +11,11 @@ type Node struct {
 	Role            string
 	TaskCount       int
 }
+
+func NewNode(name, ip, role string) *Node {
+	return &Node{
+		Name: name,
+		Ip:   ip,
+		Role: role,
+	}
+}
