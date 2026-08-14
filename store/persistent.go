974c28c0351fec5974cc91ccc649f7837a372cf1
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2026-08-15 01:28:10 +0300

    feat(store/persistent): added persistent storage for task.TaskEvent

--- store/persistent.go
+++ store/persistent.go
@@ -138,3 +138,127 @@ func (t *TaskStore) Count() (int, error) {
 
 	return taskCount, nil
 }
+
+type TaskEventStore struct {
+	Db       *bolt.DB
+	DbFile   string
+	FileMode os.FileMode
+	Bucket   string
+}
+
+var _ Store[task.TaskEvent] = (*TaskEventStore)(nil)
+
+func NewTaskEventStore(file string, mode os.FileMode, bucket string) (*TaskEventStore, error) {
+	db, err := bolt.Open(file, mode, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open %s", file)
+	}
+
+	tes := TaskEventStore{
+		Db:       db,
+		DbFile:   file,
+		FileMode: mode,
+		Bucket:   bucket,
+	}
+
+	err = tes.CreateBucket()
+	if err != nil {
+		log.Printf("bucket already exists, will use it instead of creating a new one") // ?
+	}
+
+	return &tes, nil
+}
+
+func (tes *TaskEventStore) Close() {
+	tes.Db.Close()
+}
+
+func (tes *TaskEventStore) CreateBucket() error {
+	return tes.Db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte(tes.Bucket)) // CreateBucketNotExists is a thing, should refactor for that
+		if err != nil {
+			return fmt.Errorf("create bucket %s: %w", tes.Bucket, err)
+		}
+		return nil
+	})
+}
+
+func (tes *TaskEventStore) Put(key uuid.UUID, value *task.TaskEvent) error {
+	return tes.Db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(tes.Bucket))
+
+		buf, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+
+		err = b.Put([]byte(key[:]), buf)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (tes *TaskEventStore) Get(key uuid.UUID) (*task.TaskEvent, error) {
+	var taskEvent task.TaskEvent
+	err := tes.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(tes.Bucket))
+		t := b.Get(key[:])
+		if t == nil {
+			return fmt.Errorf("task %s not found", key)
+		}
+		err := json.Unmarshal(t, &taskEvent)
+		if err != nil {
+			return fmt.Errorf("unable to unmarshal: %w", err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &taskEvent, nil
+}
+
+func (tes *TaskEventStore) List() ([]*task.TaskEvent, error) {
+	var tasks []*task.TaskEvent
+	err := tes.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(tes.Bucket))
+		b.ForEach(func(k, v []byte) error {
+			var task task.TaskEvent
+			err := json.Unmarshal(v, &task)
+			if err != nil {
+				return err
+			}
+			tasks = append(tasks, &task)
+			return nil
+		})
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func (tes *TaskEventStore) Count() (int, error) {
+	taskCount := 0
+	err := tes.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(tes.Bucket))
+		b.ForEach(func(k, v []byte) error {
+			taskCount++
+			return nil
+		})
+		return nil
+	})
+
+	if err != nil {
+		return -1, err
+	}
+
+	return taskCount, nil
+}
