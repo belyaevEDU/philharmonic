f15f0dbc1f588fad34aff2e1aef1a4c5fab10715
belyaevEDU <veniamin@belyaev.work>
2026-08-14 17:53:59 +0300

    feat(store/in_memory): added an in memory storage for taskevent

--- store/in_memory.go
+++ store/in_memory.go
@@ -43,3 +43,49 @@ func (i *InMemoryTaskStore) List() (any, error) {
 
 	return tasks, nil
 }
+
+func (i *InMemoryTaskStore) Count() (int, error) {
+	return len(i.db), nil
+}
+
+type InMemoryTaskEventStore struct {
+	db map[string]*task.TaskEvent
+}
+
+func NewInMemoryTaskEventStore() *InMemoryTaskEventStore {
+	return &InMemoryTaskEventStore{
+		db: make(map[string]*task.TaskEvent),
+	}
+}
+
+func (i *InMemoryTaskEventStore) Put(key string, value any) error {
+	e, ok := value.(*task.TaskEvent)
+	if ok {
+		return fmt.Errorf("value %v is not a task.TaskEvent type", value)
+	}
+
+	i.db[key] = e
+	return nil
+}
+
+func (i *InMemoryTaskEventStore) Get(key string) (any, error) {
+	e, ok := i.db[key]
+	if !ok {
+		return nil, fmt.Errorf("task with key %s does not exist", key)
+	}
+
+	return e, nil
+}
+
+func (i *InMemoryTaskEventStore) List() (any, error) {
+	var tasks []*task.TaskEvent
+	for _, e := range i.db {
+		tasks = append(tasks, e)
+	}
+
+	return tasks, nil
+}
+
+func (i *InMemoryTaskEventStore) Count() (int, error) {
+	return len(i.db), nil
+}
