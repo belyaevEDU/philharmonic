11e3f1e22408f120ba66f11c14991c2cbff00641
belyaevEDU <veniamin@belyaev.work>
2026-08-15 15:21:39 +0300

    feat(worker/api): added graceful shutdown; brought out timeouts into consts

--- worker/api.go
+++ worker/api.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
@@ -10,11 +12,18 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+const (
+	apiReadHeaderTimeout = 5 * time.Second
+	apiShutdownTimeout   = 10 * time.Second
+)
+
 type Api struct {
 	Address string
 	Port    int
 	Worker  *Worker
 	Router  *chi.Mux
+
+	server *http.Server
 }
 
 func (a *Api) initRouter() {
@@ -35,15 +44,24 @@ func (a *Api) initRouter() {
 func (a *Api) Start() error {
 	a.initRouter()
 
-	server := http.Server{
+	a.server = &http.Server{
 		Addr:              net.JoinHostPort(a.Address, strconv.Itoa(a.Port)),
 		Handler:           a.Router,
-		ReadHeaderTimeout: time.Second * 5,
+		ReadHeaderTimeout: apiReadHeaderTimeout,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
+	err := a.server.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		return fmt.Errorf("error raised when starting an http server: %w", err)
 	}
 	return nil
+}
+
+func (a *Api) Shutdown() error {
+	if a.server == nil {
+		return nil
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), apiShutdownTimeout)
+	defer cancel()
+	return a.server.Shutdown(ctx)
 }
