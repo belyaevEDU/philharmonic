cbf31936aaf41226389e1cfa0215a365ed90e232
belyaevEDU <veniamin@belyaev.work>
2026-08-18 00:31:51 +0300

    feat(cmd): added status command

---
+++ cmd/status.go
@@ -0,0 +1,80 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"text/tabwriter"
+	"time"
+
+	"github.com/belyaevedu/philharmonic/task"
+	"github.com/docker/go-units"
+	"github.com/spf13/cobra"
+)
+
+const (
+	timeAgoFmt = "%s ago"
+)
+
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Status command to list tasks.",
+	Long: `philharmonic status command.
+
+The status command allows a user to get the status of tasks from the Philharmonic manager.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		manager, err := cmd.Flags().GetString("manager")
+		if err != nil {
+			return err
+		}
+
+		url := fmt.Sprintf("http://%s/tasks", manager)
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			err := resp.Body.Close()
+			if err != nil {
+				log.Fatalf("Error raised closing response body: %v\n", err)
+			}
+		}()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+
+		var tasks []*task.Task
+		if err := json.Unmarshal(body, &tasks); err != nil {
+			return fmt.Errorf("error unmarshalling tasks: %w", err)
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', tabwriter.TabIndent)
+		fmt.Fprintln(w, "ID\tNAME\tCREATED\tSTATE\tCONTAINERNAME\tIMAGE\t")
+		for _, task := range tasks {
+			var start string
+			if task.StartTime.IsZero() {
+				start = fmt.Sprintf(timeAgoFmt, units.HumanDuration(time.Duration(0)))
+			} else {
+				start = fmt.Sprintf(timeAgoFmt, units.HumanDuration(time.Now().UTC().Sub(task.StartTime)))
+			}
+
+			state := task.State.String()[task.State]
+			// holy hell
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t\n", task.ID, task.Name, start, state, task.Name, task.Image)
+		}
+		if err := w.Flush(); err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd)
+	statusCmd.Flags().StringP("manager", "m", "localhost:5555", "Manager to talk to")
+}
