404bc5f14d8e44b6d4fb03069c4a3c22276b8b2f
belyaevEDU <veniamin@belyaev.work>
2026-08-17 22:26:03 +0300

    feat(cmd): added a stop command

---
+++ cmd/stop.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var stopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "Stop a running task.",
+	Long: `philharmonic stop command.
+
+The stop command stops a running task.`,
+	Args: cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		manager, err := cmd.Flags().GetString("manager")
+		if err != nil {
+			return err
+		}
+
+		url := fmt.Sprintf("http://%s/tasks/%s", manager, args[0])
+		client := &http.Client{}
+		req, err := http.NewRequest("DELETE", url, nil)
+		if err != nil {
+			return fmt.Errorf("error creating requets %s: %w", url, err)
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return fmt.Errorf("error connecting to %s: %w", url, err)
+		}
+
+		if resp.StatusCode != http.StatusNoContent {
+			return fmt.Errorf("error sending request: %w", err)
+		}
+
+		log.Printf("Task %v has been stopped.\n", args[0])
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stopCmd)
+	stopCmd.Flags().StringP("manager", "m", "localhost:5555", "Manager to taslk to")
+}
